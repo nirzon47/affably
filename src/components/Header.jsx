ff558e9e6faba6d4893c5ac3f92361d660858700
Nirzon Karmakar <nirzon47@gmail.com>
2024-01-30 19:54:38 +0530

    optimized some sections

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,14 +3,28 @@ import { Link } from 'react-router-dom'
 
 const Header = (prop) => {
 	return (
-		<header className='sticky top-0 h-16 py-4 px-4 bg-black md:h-[4.5rem] md:py-4 md:px-48 flex justify-between'>
+		<header className='sticky top-0 h-16 py-4 px-4 bg-black md:h-[4.5rem] md:py-4 md:px-48 flex justify-between items-center'>
 			<Link to={'/dashboard'}>
-				<div className='flex items-center h-full'>
-					<img src='/logo.png' alt='logo' className='h-full' />
-					<h3 className='text-3xl font-bold text-primary'>
-						<span className='opacity-0'>A</span>
-						<span className='inline-block -translate-x-6'>ffably</span>
-					</h3>
+				<div className='flex items-center h-full md:items-end gap-x-6 md:gap-x-12'>
+					<div className='flex md:items-end'>
+						<img src='/logo.png' alt='logo' className='h-10' />
+						<h3 className='hidden text-3xl font-bold text-primary md:inline-block'>
+							<span className='opacity-0'>A</span>
+							<span className='inline-block -translate-x-6'>ffably</span>
+						</h3>
+					</div>
+					<nav className='flex gap-x-4'>
+						<Link to='/'>
+							<p className='font-semibold duration-150 cursor-pointer md:text-lg hover:text-blue-500'>
+								Home
+							</p>
+						</Link>
+						<Link to='/about'>
+							<p className='font-semibold duration-150 cursor-pointer md:text-lg hover:text-blue-500'>
+								About
+							</p>
+						</Link>
+					</nav>
 				</div>
 			</Link>
 			{prop.logoutEnabled && <LogOutButton />}
--- src/components/LogOutButton.jsx
+++ src/components/LogOutButton.jsx
@@ -1,7 +1,7 @@
-import { Button } from '@mui/material'
 import { getAuth, signOut } from 'firebase/auth'
 import { useNavigate } from 'react-router-dom'
 import { toast } from 'react-toastify'
+import { MdLogout } from 'react-icons/md'
 
 const LogOutButton = () => {
 	const auth = getAuth()
@@ -20,9 +20,12 @@ const LogOutButton = () => {
 	}
 
 	return (
-		<Button variant='contained' color='error' onClick={handleLogOut}>
-			Log Out
-		</Button>
+		<button
+			className='p-2 text-white duration-150 rounded-lg md:p-3 hover:bg-red-700 bg-error'
+			onClick={handleLogOut}
+		>
+			<MdLogout className='text-xl' />
+		</button>
 	)
 }
 
